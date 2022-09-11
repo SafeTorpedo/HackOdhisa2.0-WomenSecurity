44492337fb0c4f9ec0b160e6735b733a880d5136
hanzili <hanzi.li@mail.mcgill.ca>
2022-09-10 22:14:12 -0400

    make the backend part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,100 @@
 import React from "react";
 import "./App.css";
-import icon from "./images/chatbot.png";
-import chat from "./images/chat_with_us.png";
+import chatBot from "./components/chatBot";
+
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector(".chatbox__button"),
+            chatBox: document.querySelector(".chatbox__support"),
+            sendButton: document.querySelector(".chatbox__send"),
+        };
+
+        this.status = false;
+        this.reply = [];
+    }
+
+    toggle(chatbox) {
+        this.status = !this.status;
+        if (this.status) {
+            chatbox.classList.add("chatbox--active");
+        } else {
+            chatbox.classList.remove("chatbox--active");
+        }
+    }
+
+    onSend(chatbox) {
+        var textField = chatbox.querySelector('input');
+        let text1 = textField.value
+        if (text1 === "") {
+            return;
+        }
+
+        let msg1 = { name: "User", message: text1 }
+        this.reply.push(msg1);
+        //$SCRIPT_ROOT is declared in a html file in the tutorial
+        fetch($SCRIPT_ROOT + '/predict', {
+            method: 'POST',
+            body: JSON.stringify({ message: text1 }),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(r => r.json())
+            .then(r => {
+                let msg2 = { name: "woman security bot", message: r.answer };
+                this.message.push(msg2);
+                this.updateChat(chatbox)
+                textField.value = ''
+
+            }).catch((error) => {
+                console.error('Error:', error);
+                this.updateChat(chatbox)
+                textField.value = ''
+            });
+
+    }
+
+    updateChat(chatbox) {
+        var html = '';
+        this.reply.slice().reverse().forEach(function(item, ) {
+            if (item.name === "woman security bot")
+            {
+                html += '<div>' + item.message + '</div>'
+            }
+            else 
+            {
+                html += '<div>' + item.message + '</div>'
+            }
+        });
+
+        //const chatboxreply = chatbox.querySelector('.chatbox__messages');
+        //TO DO: these html need to be append to the main file
+    }
+
+    display() {
+        const { openButton, chatBox, sendButton } = this.args;
+        openButton.addEventListener("click", () => this.toggle(chatBox));
+        sendButton.addEventListener("click", () => this.onSend(chatBox)); //Hanzii
+
+        const node = chatBox.querySelector("input");
+        node.addEventListener("keyup", ({ key }) => {
+            if (key === "Enter") {
+                this.onSend(chatBox);
+            }
+        });
+    }
+}
 
 const App = () => {
-    return React.createElement("div", { className: "container" }, [
-        React.createElement("div", { className: "chatbox" }, [
-            React.createElement("div", { className: "chatbox__support" }, [
-                React.createElement("div", { className: "chatbox__header" }, [
-                    React.createElement(
-                        "div",
-                        { className: "chatbox__header__icon" },
-                        [
-                            React.createElement("img", {
-                                src: icon,
-                                alt: "chatbot",
-                            }),
-                        ]
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "chatbox__header__content" },
-                        [
-                            React.createElement(
-                                "h4",
-                                {
-                                    className:
-                                        "chatbox__header__content__title",
-                                },
-                                "Chat Support"
-                            ),
-                            React.createElement(
-                                "p",
-                                { className: "chatbox__header__description" },
-                                "Hi, I'm gonna assist you if you need any help. Rest assured"
-                            ),
-                        ]
-                    ),
-                ]),
-                React.createElement("div", { className: "chatbox__messages" }, [
-                    React.createElement("div"),
-                ]),
-                React.createElement("div", { className: "chatbox__footer" }, [
-                    React.createElement("input", {
-                        type: "text",
-                        placeholder: "Ask me anything!",
-                    }),
-                    React.createElement(
-                        "button",
-                        { className: "chatbox__footer__send send__button" },
-                        "Send"
-                    ),
-                ]),
-            ]),
-            React.createElement("div", { className: "chatbox__button" }, [
-                React.createElement("button", {}, [
-                    React.createElement("img", { src: chat }),
-                ]),
-            ]),
-        ]),
+    //creating object
+    return React.createElement("div", { className: "App" }, [
+        React.createElement(chatBot),
     ]);
+    // To be instated after completing the methods
+    // const chatbox = new Chatbox();
+    // chatbox.display();
 };
 
 export default App;
